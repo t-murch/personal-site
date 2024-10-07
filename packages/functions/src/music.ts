13bb00c9be7766a0ca98e206d5cb3ca22adcdca8
toddmurch <t.murchjr@gmail.com>
2024-10-07 14:42:48 -0500

    basic test outline.

--- packages/functions/src/music.ts
+++ packages/functions/src/music.ts
@@ -2,5 +2,5 @@ import handler from "@portfolio-tm/core/handler";
 import * as Music from "@portfolio-tm/core/music";
 
 export const popular = handler(async (_event) => {
-  return JSON.stringify(await Music.popularPlaylist());
+  return JSON.stringify(await Music.popularPlaylist(Music.getAccessToken));
 });
